1350120c6921e9b6b4ada6016856e10980b2c07b
JamesEdwardsMag <JamesEdwardsMag>
2023-02-20 14:21:11 +0000

    Added some basic Python

---
+++ Day1.py
@@ -0,0 +1,37 @@
+# Databricks notebook source
+print("hello world")
+
+# COMMAND ----------
+
+#Strings
+first_name = "James"
+second_name = "Edwards"
+
+#Booleans
+bool1 = True
+bool2 = False
+
+#Integers
+int1 = 10
+int2 = 100
+int3 = -100
+
+#floats
+float1 = 1.5
+float2 = -1.5
+
+#list
+list1 = [50, 100, 150, 200, 250]
+list_product = ["clock", "car", "bus"]
+
+print(list_product)
+
+print(first_name + " " +second_name)
+
+# COMMAND ----------
+
+number1 = input("please enter number")
+number2 = input("enter a second")
+
+print(int(number1)+int(number2))
+
