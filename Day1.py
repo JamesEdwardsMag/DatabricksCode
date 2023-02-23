a3d56696c1a2e6b4280e14a912b9a7e3b4d8bfad
JamesEdwardsMag <JamesEdwardsMag>
2023-02-23 09:32:28 +0000

    update

--- Day1.py
+++ Day1.py
@@ -363,7 +363,7 @@ num = random.randint(0,100)
 
 while guess != num:
     guess =int(input("Guess the number"))
-    
+   
     if guess == num:
            print("Good guess!")
                             
@@ -372,4 +372,33 @@ while guess != num:
                     
     else:
            print("Lower...")
-  
+
+
+# COMMAND ----------
+
+#Good example of using if, elif and else functions 
+price = 50
+quantity = 5
+amount = price*quantity
+print (amount)
+
+if amount > 100:
+    if amount > 500:
+        print("Amount is more than 500")
+        
+    else:
+        if amount < 500 and amount > 400:
+            print("Amount is between 400 and 500")
+            
+        elif amount < 500 and amount > 300:
+                print("Amount is between 300 and 500")
+        
+        else: 
+                print("Amount is between 200 and 500")
+        
+elif amount == 100: 
+     print("Amount is 100")
+        
+else:
+     print("Amount is less than 100")
+ 
